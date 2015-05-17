bb982d7be73cec4c7a32df67256842ad008e044f
Nat Budin <nbudin@patientslikeme.com>
2015-05-17 13:36:17 -0400

    Regen schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150517172842) do
+ActiveRecord::Schema.define(version: 20150517165436) do
 
   create_table "projects", force: :cascade do |t|
     t.text     "title"
     t.text     "authors"
     t.text     "license"
     t.text     "description"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
