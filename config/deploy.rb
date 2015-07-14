ab1d9ed062021cc02891512a6fb1c904e8fd33d2
Nat Budin <nbudin@patientslikeme.com>
2015-07-14 09:00:41 -0400

    Deploy fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,10 +28,11 @@ set :log_level, :info
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/application.yml')
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
