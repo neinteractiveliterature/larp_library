71aecd84be47717c9fc97f963fb3d0e5987550c8
Nat Budin <nbudin@patientslikeme.com>
2015-07-14 09:13:55 -0400

    Do something useful if user isn't signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from CanCan::AccessDenied do |exception|
+    if user_signed_in?
+      redirect_to root_url, alert: exception.message
+    else
+      session[:user_return_to] = request.url
+      redirect_to new_user_session_url
+    end
+  end
 end
