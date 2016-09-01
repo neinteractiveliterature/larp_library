82928cd4e87eb6799caec8b47182d88a77ee16e0
Nat Budin <nbudin@patientslikeme.com>
2016-09-01 10:20:46 -0400

    Show tag browser on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@ class HomeController < ApplicationController
 
   def index
     @project_promotions = ProjectPromotion.includes(:project => :brand).order("projects.title")
+    @tags_by_category = Tag.includes(:tag_category, :projects).order(:name).group_by(&:tag_category)
   end
 end
