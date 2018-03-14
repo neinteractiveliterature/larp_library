a8c5c502f81f6ad84c30cda5daa81e63dacee313
Nat Budin <nbudin@patientslikeme.com>
2018-03-14 16:52:45 -0700

    Set up Mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,15 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'library.aegames.org' }
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.default_url_options = { host: 'library.interactiveliterature.org' }
 end
+
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'library.interactiveliterature.org',
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
