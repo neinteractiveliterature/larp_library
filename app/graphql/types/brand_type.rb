ede00d544498223612da063d33b31e5ddfba2f66
Nat Budin <natbudin@gmail.com>
2021-05-14 15:46:27 -0700

    Authorization stuff

--- app/graphql/types/brand_type.rb
+++ app/graphql/types/brand_type.rb
@@ -8,10 +8,11 @@ module Types
     field :description, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :approved, Boolean, null: false
-    field :creator_id, Integer, null: true
+    field :approved, Boolean, required_permission: :manage, null: true
     field :projects, Types::ProjectType.connection_type, null: false
+    field :creator, Types::UserType, null: true
+    field :users, [Types::UserType], null: false
 
-    association_loader Brand, :projects
+    association_loaders Brand, :projects, :creator, :users
   end
 end
