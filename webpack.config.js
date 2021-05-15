08fb96132a9d870d3902dbb3d40f8147bd42c6ff
Nat Budin <natbudin@gmail.com>
2021-05-15 12:54:49 -0700

    Fix placeholder name

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ const config = {
     filename: 'js/[name]-[contenthash].js',
     chunkFilename: 'js/[name]-[contenthash].chunk.js',
     hotUpdateChunkFilename: 'js/[id]-[chunkash].hot-update.js',
-    assetModuleFilename: 'static/[fullhash][ext][query]',
+    assetModuleFilename: 'static/[contenthash][ext][query]',
     path: path.resolve(__dirname, './public/packs'),
     publicPath: ASSET_PATH,
   },
