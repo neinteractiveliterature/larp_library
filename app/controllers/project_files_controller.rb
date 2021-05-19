33708885e142157a7c5d1fc866f6ff4fafd87f5e
Nat Budin <natbudin@gmail.com>
2021-05-19 09:26:00 -0700

    Remove unused actions

--- app/controllers/project_files_controller.rb
+++ app/controllers/project_files_controller.rb
@@ -5,28 +5,11 @@ class ProjectFilesController < ApplicationController
 
   respond_to :json
 
-  def create
-    @project_file.uploader = current_user
-    @project_file.save
-
-    respond_with @brand, @project, @project_file
-  end
-
-  def destroy
-    @project_file.destroy
-    head :ok
-  end
-
   def auth_upload
     digest = OpenSSL::Digest.new('sha1')
     hmac = OpenSSL::HMAC.digest(digest, ENV['AWS_SECRET_ACCESS_KEY'].encode('UTF-8'), request['to_sign'].encode('UTF-8'))
     hmac64 = Base64.encode64(hmac).chomp
 
     render plain: hmac64, status: 200
-  end
-
-  private
-  def project_file_params
-    params.require(:project_file).permit(:url, :filename, :filesize, :filetype, :filepath)
   end
 end
