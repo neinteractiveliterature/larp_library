f9dafabfce1b94ebf886aea1a03411401c68b4fd
Nat Budin <natbudin@gmail.com>
2021-05-27 12:05:19 -0700

    Set CORS and caching headers on static files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,12 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  config.public_file_server.headers = {
+    'Access-Control-Allow-Origin' => '*',
+    'Cache-Control' => 'public, max-age=15552000',
+    'Expires' => "#{1.year.from_now.to_formatted_s(:rfc822)}"
+  }
+
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
