4b68ab1db5a2a7926fd00fd69593c06f60ccf82d
Nat Budin <natbudin@gmail.com>
2021-05-27 12:44:06 -0700

    gzip server-rendered content

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,8 @@ module LarpLibrary
     config.rack_cas.server_url = 'https://accounts.sugarpond.net/cas'
     config.rack_cas.service = '/users/service'
 
+    config.middleware.use Rack::Deflater
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
